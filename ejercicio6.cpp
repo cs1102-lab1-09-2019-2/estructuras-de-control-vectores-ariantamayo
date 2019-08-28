ed203bd36beead29f69ba01016caa0c2dae0f3f4
Arián Tamayo <54276418+ariantamayo@users.noreply.github.com>
2019-08-28 15:34:53 -0500

    exercise 6 solved

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,19 @@
-/*
+#include<iostream>
+#include<algorithm>
+#include<vector>
 
- Leer tres numeros del usuario separados por espacio: 10 15 20
- Y encuentra el mayor valor: 30
+using namespace std;
 
- */
+int main(){
+  int numeros;
+  vector <int> lista;
+  for(int i=0;i<3;i++){
+   cout << "ingrese 3 numeros"; 
+   cin>>numeros;
+   lista.push_back(num);
+  }
+  sort(lista.begin(), lista.end());
+  cout<<"el numero mayor de su lista es: "<< lista[2];
+ 
+ return 0;
+}
