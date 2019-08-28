d2c0e3bf7a743fce2c2963bd5453e5495ac8955f
Arián Tamayo <54276418+ariantamayo@users.noreply.github.com>
2019-08-28 17:43:04 -0500

    exercise 5 solved

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -23,6 +23,27 @@
 using namespace std;
 
 int main() {
-    float a, b, c, delta, x1, x2;
-
+    double a, b, c, delta, x1, x2;
+    cout<<"Escribe los 3 parametros separados por un espacio en este orden : a b c: ";
+    cin>>a>>b>>c;
+    if(a==0){
+      if(b==0)
+      cout<<"La ecuacion no tiene solución";
+      else{
+      x1=-c/b;
+      cout<<"la solución es"<<x1;}
+      }
+    delta=pow(b,2)-(4*a*c);
+    if(delta<0)
+      cout<<"La ecuación no tiene solucion";
+    else{
+      x1=(-b+(pow(delta,0.5)))/(2*a);
+      x2=(-b-(pow(delta,0.5)))/(2*a);
+      if(x1==x2)
+        cout<<"La solucion es "<<x1;
+      else{
+        cout<<"Existen dos soluciones y son: "<<x1<<" y "<<x2;
+      }
+    }
+    return 0;
 }
