bab15e969565a3779bb24fa2fbad2ff144f141f9
Arián Tamayo <54276418+ariantamayo@users.noreply.github.com>
2019-08-30 21:40:25 -0500

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,41 +1,47 @@
-#include <iostream>
 #include <vector>
-#include <iomanip>
-
+#include <iostream>
 using namespace std;
-
 int moda(vector<int> &lista);
-vector<int> leerConsola();
+vector<int> leerConsole();
 
-vector<int> leerConsola() {
-    vector<int> valores;
-    int num;
-    while(true) {
-        cin >> num;
-        if(num != -1)
-            valores.push_back(num);
-        else
-            break;
-    }
-    return valores;
+vector<int> leerConsole() {
+    //Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros:
+    vector<int> elementos;
+    int numero;
+    do {
+        cout << "Ingrese un entero positivo, escriba -1 si ya no quiere ingresar mas numeros:";
+        cin >> numero;
+        elementos.insert(elementos.end(), numero);
+
+    } while (numero != -1);
+    return elementos;
 }
 
 int moda(vector<int> &lista) {
-    vector <int> frecuencia;
-    while(true)
-        for(int i = 0; i < lista.size(); i++){
-        if (lista[i] == lista [i])
-            frecuencia.push_back(lista[i]);
-        else
-            break;
-      
+    /*
+    La moda es el elemento que mas se repite en la lista de elementos
+     */
+    int mayor,indicemoda;
+    for(int i=0 ; i<lista.size() ; i++){
+        int numerorepeticiones=0;
+        for(int j=0 ; j<lista.size() ; j++){
+            if(lista[i]==lista[j] && i!=j){
+                numerorepeticiones++;
+            }
+        }
+        if(numerorepeticiones>= mayor){
+            mayor=numerorepeticiones;
+            indicemoda=i;
+        }
     }
-    return 0;
+    return lista[indicemoda];
 }
+
 
 int main() {
     cout<<"Calcular la moda de los numeros ingresados:\n\n";
-    vector<int> leer_usuario = leerConsola();
+    vector<int> leer_usuario = leerConsole();
     int moda_elemento = moda(leer_usuario);
     cout<<"La moda es: "<<moda_elemento<<"\n";
+    return 0;
 }
